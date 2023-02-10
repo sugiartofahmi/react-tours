7dcf550a11046f4fafd35a021429d7c1821130d8
sugiartofahmi <sugiartofahmi@gmail.com>
2023-02-10 21:14:05 +0700

    Fahmi Sugiarto | Complete Project

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,8 +1,13 @@
+import GetTours from "../../modules/Home/GetTours";
 import MainLayout from "../../layout/MainLayout";
+
 const Home = () => {
   return (
     <MainLayout>
-      <h1>Home</h1>
+      <h1 className="font-bold text-4xl underline decoration-blue-400 underline-offset-8">
+        Our Tours
+      </h1>
+      <GetTours />
     </MainLayout>
   );
 };
